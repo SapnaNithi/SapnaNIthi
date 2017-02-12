782ead48339ed8ce01f8fc6bd438a7b58350f2ae
SapnaNithi <sapnavasu540@gmail.com>
2017-02-12 19:55:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,40 +6,13 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
-});
-
-app.get('/article-one', function (req, res) {
-    res.send('article one hehe has come');
-});
-
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
+  res.sendFile(path.join(__dirname, 'helo', 'index.html'));
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
 
-app.get('/ui/wall.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'wall.jpg'));
-});
-
-app.get('/ui/camper.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'camper.jpg'));
-});
-
-app.get('/ui/my-file', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'my-file.html'));
-});
-
-app.get('/kutta', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'kutta.html'));
-});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
